074eb1e9c1b4bb21499ee5480ce30cba6e0a442a
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2012-10-12 23:33:46 -0500

    added UART code

---
+++ UART/Debug/UARTtranseiver.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+UARTtranseiver.obj: ../UARTtranseiver.c
+UARTtranseiver.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h
+UARTtranseiver.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+UARTtranseiver.obj: C:/ti/ccsv5/ccs_base/msp430/include/calibrations.h
+
+../UARTtranseiver.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/calibrations.h: 
---
+++ UART/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2231.cmd 
+
+C_SRCS += \
+../UARTtranseiver.c \
+../main.c 
+
+OBJS += \
+./UARTtranseiver.obj \
+./main.obj 
+
+C_DEPS += \
+./UARTtranseiver.pp \
+./main.pp 
+
+C_DEPS__QUOTED += \
+"UARTtranseiver.pp" \
+"main.pp" 
+
+OBJS__QUOTED += \
+"UARTtranseiver.obj" \
+"main.obj" 
+
+C_SRCS__QUOTED += \
+"../UARTtranseiver.c" \
+"../main.c" 
+
+
