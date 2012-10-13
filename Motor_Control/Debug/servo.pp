879898c4cb09c4cdded7eb845a9e4e41a9735334
Kevin Gilbert <K.Gilbert.CH@gmail.com>
2012-10-13 15:35:22 -0500

    Motor_Control directory with servo PWM generator added

---
+++ Motor_Control/Debug/servo.pp
@@ -0,0 +1,9 @@
+# FIXED
+
+servo.obj: ../servo.c
+servo.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h
+servo.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+
+../servo.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2231.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
---
+++ Motor_Control/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430g2231.cmd 
+
+C_SRCS += \
+../main.c \
+../servo.c 
+
+OBJS += \
+./main.obj \
+./servo.obj 
+
+C_DEPS += \
+./main.pp \
+./servo.pp 
+
+C_DEPS__QUOTED += \
+"main.pp" \
+"servo.pp" 
+
+OBJS__QUOTED += \
+"main.obj" \
+"servo.obj" 
+
+C_SRCS__QUOTED += \
+"../main.c" \
+"../servo.c" 
+
+
